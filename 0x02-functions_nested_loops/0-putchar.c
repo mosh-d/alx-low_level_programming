848fc6bb76227523dd6979eba28cb6495f79c477
mosh-d <shopsy8@gmail.com>
2022-10-18 19:43:40 +0000

    Updating file 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Prints out "_putchar" using putchar
+ * main - program that prints out putchar
  *
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(void)
 {
+/*First we create an array*/
 	char word[] = "_putchar";
 
 	for (int count = 0; count < MAXSTRING; count++)
