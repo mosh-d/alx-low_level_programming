1ea9a070a0c2f9db084777da9b0d852befd03794
mosh-d <shopsy8@gmail.com>
2022-10-19 21:10:26 +0000

    commiting 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,6 +3,7 @@
 /**
  * print_times_table - Prints the times table of the input,
  *                     starting with 0.
+ * @x: The value of the times table to be printed.
  */
 void print_times_table(int x)
 {
