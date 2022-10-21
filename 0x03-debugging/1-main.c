fa5b58938e849a65bafe9d55d5fb4cafdd792d8d
mosh-d <shopsy8@gmail.com>
2022-10-21 19:55:12 +0000

    updating 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - causes an infinite loop
+*
 * Return: 0
 */
 
@@ -13,10 +14,13 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)
+/*
+*
+	while (i < 10)
 	{
 		putchar(i);
-	}*/
+	}
+*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
